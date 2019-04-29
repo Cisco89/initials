55404f6b6a4817aa26166a034184aab0301fcd8b
Cisco89 <franciscoadriangarcia1@gmail.com>
2019-04-29 02:00:14 -0600

    Completed initials assignment

--- initials.py
+++ initials.py
@@ -1,9 +1,14 @@
 def get_initials(fullname):
     """ Given a person's name, returns the person's initials (uppercase) """
     # TODO your code here
-    my_initials = ''.join([s[:1] for s in fullname.split(' ')])
-    my_initials= my_initials.upper()
-    return my_initials
+    my_initials = ''
+    for index in fullname.split(' '):
+        my_initials += index[0]
+    return my_initials.upper()
+    
+def main():
+    initials = get_initials(input("What is your name?"))
+    print("The initials to your full name are", initials)
 
-initials = get_initials("francisco garcia")
-print("The initials to your full name are", initials)
+if __name__ == '__main__':
+    main()
